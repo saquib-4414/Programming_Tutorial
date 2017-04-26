2af83e518ab4714105b1f8fd9471db79965ddc73
saquib-4414 <justnazmus@gmail.com>
2017-04-26 13:55:30 +0200

    evenodd.java completed

--- Structures/src/EvenOdd.java
+++ Structures/src/EvenOdd.java
@@ -6,10 +6,20 @@ public class EvenOdd {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+		System.out.print("give your number ");
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
 		
-	}
-	
+		int sc=scan.nextInt();
+		System.out.println("print all odd numbers untill "+sc);
+		
+		for(int i=1;i<=sc;i++)
+			if(i%2==1)
+			{
+				
+				System.out.print(" "+i);
+			}
+		
+		
+}
 }
